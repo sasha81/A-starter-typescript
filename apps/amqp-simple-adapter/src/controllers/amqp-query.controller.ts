c3dc639d8be78944e1568963f6dd772eb9568980
sasha81 <sashaeliseev81@gmail.com>
2025-01-25 16:03:45 +0300

    group update added to amqp-simple controller

--- apps/amqp-simple-adapter/src/controllers/amqp-query.controller.ts
+++ apps/amqp-simple-adapter/src/controllers/amqp-query.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Logger } from "@nestjs/common";
-import {  MessagePattern, Payload } from "@nestjs/microservices";
-import { ArgumentOutOfRangeError } from "rxjs";
+import {  EventPattern, MessagePattern, Payload } from "@nestjs/microservices";
 import { AmqpAdapterService } from "../services/amqp-adapter.service";
-
+import { GlobalGroupUpdatedEvent } from '@libs/commons/events/GroupUpdatedEvent';
 import {  AmqpUserViewDto } from "../dto/users-dto";
+import { UsersUpdateService } from '@libs/users/users.update.service'
 
 
 
@@ -11,16 +11,22 @@ import {  AmqpUserViewDto } from "../dto/users-dto";
 export class AmqpAdapterQueryController {
   private readonly logger = new Logger(AmqpAdapterQueryController.name);
   constructor(
+    private readonly userService: UsersUpdateService,
     private readonly service: AmqpAdapterService
   ) {}
   
-  @MessagePattern({ query: 'find-all-users' })
-  async queryAllUser(@Payload() data: string): Promise<AmqpUserViewDto[]> {
-    
-    // throw new ArgumentOutOfRangeError();
-    const result = await this.service.getAllUsers()
-    this.logger.log('find-all-users   response: ', result);
-    return result
-  }
+    @MessagePattern({ query: 'find-all-users' })
+    async queryAllUser(@Payload() data: string): Promise<AmqpUserViewDto[]> {
+      
+      // throw new ArgumentOutOfRangeError();
+      const result = await this.service.getAllUsers()
+      this.logger.log('find-all-users   response: ', result);
+      return result
+    }
 
+    @EventPattern({event: 'group-updated'})
+    async groupUpdated(@Payload() groupUpdateEvent: GlobalGroupUpdatedEvent): Promise<void>{
+      this.logger.log('group-updated event: ',groupUpdateEvent)
+      await this.userService.updateGroup(groupUpdateEvent)
+    }
   }
